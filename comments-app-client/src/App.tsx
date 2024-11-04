555448cf84145df50d57e82d68f989f3808632ca
Arsenii Yurchenko <arseniiyurchenko@Arseniis-MacBook-Air.local>
2024-11-04 09:21:53 +0200

    fix html validation. Add image displaying

--- comments-app-client/src/App.tsx
+++ comments-app-client/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Home from "./pages/Home";
+import 'lightbox2/dist/css/lightbox.min.css';
+import 'lightbox2/dist/js/lightbox-plus-jquery.min.js';
 
 function App() {
   return (
