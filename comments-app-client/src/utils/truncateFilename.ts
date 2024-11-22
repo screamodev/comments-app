800aab10fecad4e7ba6b9ad245c4bf7d9eecdd87
Arsenii Yurchenko <arseniiyurchenko@Arseniis-MacBook-Air.local>
2024-11-22 11:00:24 +0200

    Fix eslint error

--- comments-app-client/src/utils/truncateFilename.ts
+++ comments-app-client/src/utils/truncateFilename.ts
@@ -1,4 +1,4 @@
-export const truncateFilename = (filename: string, maxLength: number = 5) => {
+export const truncateFilename = (filename: string, maxLength = 5) => {
     return filename.length > maxLength
         ? filename.slice(0, maxLength) + '...'
         : filename
